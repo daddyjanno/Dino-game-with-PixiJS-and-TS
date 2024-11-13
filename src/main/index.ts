07b9749e5005c70b52da63523ef8c9e399ad1574
daddyjanno <jn.drugmand@gmail.com>
2024-11-13 14:11:15 +0100

    setup

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,2 @@
-import { Person } from './types/Person'
-
-class GreaterService {
-    great(person: Person) {
-        console.log(`Hi ${person.firstname} ${person.lastname}`)
-    }
-}
-
-const person: Person = {
-    firstname: 'J-N',
-    lastname: 'Drugmand',
-}
-new GreaterService().great(person)
+const message: string = 'Hello world by Ahsan!'
+console.log(message)
--- src/main/types/Person.ts
+++
@@ -1,4 +0,0 @@
-export interface Person {
-    firstname: string
-    lastname: string
-}
